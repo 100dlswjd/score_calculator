8375fbb27ea6a4e438678de198685feffa930ff7
100dlswjd <100dlswjd@naver.com>
2024-12-03 15:52:15 +0900

    체크박스 이미지 적용

--- score_widget.py
+++ score_widget.py
@@ -2,12 +2,28 @@ from PySide6.QtWidgets import QApplication, QWidget
 
 from ui.score_widget_form import Ui_score_form
 
+def resource_path(relative_path):
+    import sys
+    import os
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path).replace("\\", "/")
+
 class ScoreWidget(QWidget, Ui_score_form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         
         self.participate = False
+        
+        # 체크박스 이미지 변경
+        self.checkbox_styleSheet = "QCheckBox::indicator:checked {image: url(" + resource_path("check_icon.png") + ");} QCheckBox::indicator:unchecked {image: url(" + resource_path("x_icon.png") + ");}" 
+        
+        self.checkBox_name.setStyleSheet(self.checkbox_styleSheet)
+        
+        
         self.pushButton_m.setEnabled(self.participate)
         self.pushButton_p.setEnabled(self.participate)
         self.checkBox_name.stateChanged.connect(self.checkbox_change)
@@ -29,6 +45,7 @@ class ScoreWidget(QWidget, Ui_score_form):
             self.participate = True
         else:
             self.participate = False
+            
         self.pushButton_m.setEnabled(self.participate)
         self.pushButton_p.setEnabled(self.participate)
         
@@ -37,3 +54,4 @@ if __name__ == "__main__":
     window = ScoreWidget()
     window.show()
     app.exec()
+    
