bedc90dacd322219fffdd4d95935bbca4b8c12dd
100dlswjd <100dlswjd@naver.com>
2024-12-03 11:54:07 +0900

    컴럼 숫자만큼 동적으로 작동하게 변경

--- test_code/test.py
+++ test_code/test.py
@@ -1,21 +1,32 @@
 import sqlite3
 
 conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
+cur = conn.cursor()
 
-cursor.execute("SELECT data, data2, data3, data4, data5 FROM test")
-rows = cursor.fetchall()
+# 컬럼이름 가져와서 sql문으로 만들기
+cur.execute("PRAGMA table_info(test)")
+rows = cur.fetchall()
+names = []
+for row in rows:
+    names.append(row[1])
+names = names[3:]
+
+sql = "SELECT "
+for name in names:
+    sql += f"{name}, "
+sql = sql[:-2] + " FROM test"
+
+cur.execute(sql)
+rows = cur.fetchall()
 
 cumulative_data = {}
 
 for row in rows:
-    data1, data2, data3, data4, data5 = row
-
     pattern = tuple(col is not None for col in row)
-
+    
     if pattern not in cumulative_data:
         cumulative_data[pattern] = {
-            'cumulative_values': [0, 0, 0, 0, 0],  # Initialize with zeros for each data column
+            'cumulative_values': [0 for x in range(len(names))],  # Initialize with zeros for each data column
             'count': 0  # Initialize counter to 0
         }
 
